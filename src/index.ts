00f53d2c95188f6a042f2cb5c41abe054bbc5262
ruzkypazzy <78404403+ruzkypazzy@users.noreply.github.com>
2026-05-31 16:44:45 +0100

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// Real Brain - Main Entry Point
+export * from "./types/memory-types";
+export * from "./types/action";
+export * from "./tools/real_brain/memory-core";
+export * from "./tools/real_brain/memory-operations";
+export * from "./tools/real_brain/contextual-suggestions";
+export { createRealBrainTools } from "./langchain/real_brain/real-brain-tool";
+export * from "./actions/real_brain";
